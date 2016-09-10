a5913f8c2120bd1961cd2087fb19a2a08faceaae
Hans Løken <hans@ebamse.net>
2016-09-10 16:57:13 +0200

    Nicer(?) singleton(!) implementation

--- CharacterSheetApp/src/CharacterSheetApp/GlobalVariables.cs
+++ CharacterSheetApp/src/CharacterSheetApp/GlobalVariables.cs
@@ -5,16 +5,7 @@ namespace CharacterSheetApp
 {
 	public static class GlobalVariables
 	{
-		private static List<Models.Character> _charactersInstance;
-		public static List<Models.Character> Characters
-		{
-			get
-			{
-				if (_charactersInstance == null)
-					_charactersInstance = new List<Character>();
-
-				return _charactersInstance;
-			}
-		}
+		public static List<Models.Character> Characters { get; set; }
+			= new List<Character>();
 	}
 }
